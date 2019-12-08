e83f1d39b0297e65ebcacfc7e326a4625f9d5382
Manjari Maheshwari <50533138+manjarimaheshwari@users.noreply.github.com>
2019-12-08 04:17:59 -0800

    created ArrayList of Expenses & updated buttons

--- ExpensePage.java
+++ ExpensePage.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -17,6 +18,7 @@ public class ExpensePage extends JPanel {
 	
 	private JTextField textField1;
 	private JTextField textField2;
+	private String expenseType;
 	private Expense expense;
 	//private BudgetAmount ba;
 
@@ -36,7 +38,7 @@ public class ExpensePage extends JPanel {
 		JLabel lblNewLabel_1 = new JLabel("TYPE OF EXPENSE:");
 		lblNewLabel_1.setForeground(new Color(0, 100, 0));
 		lblNewLabel_1.setFont(new Font("Georgia", Font.PLAIN, 18));
-		lblNewLabel_1.setBounds(37, 307, 191, 16);
+		lblNewLabel_1.setBounds(37, 117, 191, 16);
 		add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("EXPENSE NAME:");
@@ -48,17 +50,19 @@ public class ExpensePage extends JPanel {
 		JLabel lblNewLabel_3 = new JLabel("EXPENSE AMOUNT:");
 		lblNewLabel_3.setForeground(new Color(0, 100, 0));
 		lblNewLabel_3.setFont(new Font("Georgia", Font.PLAIN, 18));
-		lblNewLabel_3.setBounds(37, 117, 191, 16);
+		lblNewLabel_3.setBounds(37, 307, 191, 16);
 		add(lblNewLabel_3);
 		
 		textField1 = new JTextField();
-		textField1.addActionListener(new ActionListener() {
+		
+		/* 
+		 textField1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event)
 			{
 				expense.setExpenseName(textField1.getText());
 			}
 		});
-
+		 */ 
 		textField1.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
 		
 		textField1.setHorizontalAlignment(SwingConstants.LEFT);
@@ -67,19 +71,21 @@ public class ExpensePage extends JPanel {
 		textField1.setColumns(20);
 		
 		textField2 = new JTextField();
-		/*
+	/*
 		textField2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event)
 			{
 				expense.setExpenseAmount(textField2.getText());
+				System.out.println(getExpense());
 			}
 		});
-		*/
+	*/
 		textField2.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
 		textField2.setHorizontalAlignment(SwingConstants.LEFT);
 		textField2.setBounds(210, 303, 153, 26);
 		add(textField2);
 		textField2.setColumns(20);
+		
 		
 		/*
 		JButton btnNewButton4 = new JButton("FINISH");
@@ -98,22 +104,39 @@ public class ExpensePage extends JPanel {
 		*/
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setMaximumRowCount(6);
+		comboBox.setMaximumRowCount(7);
 		comboBox.setBackground(Color.WHITE);
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Housing", "Transportaion", "Recreational", "Education", "Medical", "Food"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Select One", "Housing", "Transportation", "Recreational", "Education", "Medical", "Food"}));
 		comboBox.setBounds(222, 115, 208, 27);
 		add(comboBox);
+	
 		comboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event)
 			{
-				expense.setExpenseType(comboBox.getSelectedItem().toString());
+				expenseType = comboBox.getSelectedItem().toString(); 
 			}
 		});
-	}
+		
+		}
 	
 	public JTextField getTextField2()
 	{
 		return textField2;
 	}
 	
+	public JTextField getTextField1()
+	
+	{
+		return textField1; 
+	}
+	
+	public String getType()
+	{
+		return expenseType; 
+	}
+	
+	public String getExpense()
+	{
+		return new String(expense.getExpenseType() + ": " + expense.getExpensename() + " " + expense.getExpenseAmount()); 
+	}
 }
